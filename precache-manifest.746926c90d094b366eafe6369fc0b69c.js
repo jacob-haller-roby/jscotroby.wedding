6a469d7414d2a87870b158b3326c5d6e14d02bf0
jroby <jroby@jroby-stywl.fios-router.home>
2018-11-25 12:16:32 -0500

    Updates

---
+++ precache-manifest.746926c90d094b366eafe6369fc0b69c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e8f66860b59ee38ea6c",
+    "url": "/wedding/static/css/main.bd792461.chunk.css"
+  },
+  {
+    "revision": "5e8f66860b59ee38ea6c",
+    "url": "/wedding/static/js/main.5e8f6686.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/wedding/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "0e44ce2450fbf11c30d5",
+    "url": "/wedding/static/js/runtime~main.0e44ce24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wedding/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ebf8e4ab49e21e6280765c97e5262397",
+    "url": "/wedding/index.html"
+  }
+];
