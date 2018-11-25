5a2fd5351465912530284026a8f47c56cc0d434a
jroby <jroby@jroby-stywl.fios-router.home>
2018-11-25 13:40:43 -0500

    React Router Setup

---
+++ src/Router.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+const Index = () => <div>INDEX</div>;
+const About = () => <div>ABOUT</div>;
+const Users = () => <div>USERS</div>;
+
+const AppRouter = () => (
+    <Router>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about/">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/users/">Users</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Route path="/" exact component={Index}/>
+            <Route path="/about/" component={About}/>
+            <Route path="/users/" component={Users}/>
+        </div>
+    </Router>
+);
+
+export default AppRouter;
